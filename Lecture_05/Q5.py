90c95854fb7d15b7a285de2cb04d3e9611d2313f
Robert A. Fulkerson <rfulkerson@unomaha.edu>
2025-09-10 10:37:44 -0500

    Adding Lecture 5

---
+++ Lecture_05/Q5.py
@@ -0,0 +1,9 @@
+import math
+
+base = int(input("Enter base: "))
+pwr = float(input("Enter power: "))
+result = math.pow(base, pwr)
+#result2 = base ** pwr
+#print(f"{base} to the {pwr} power is {result} or {result2}")
+
+print(f"{base} to the {pwr} power is {result}")
---
+++ Lecture_05/Q7.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a value: "))
+y = 85.0
+z = x * 10
+y = z - 12
+print(x, y, z)
+2
