9cb45c66cd6c7b9acede7d7cbf75b7a705896320
Robert A. Fulkerson <rfulkerson@unomaha.edu>
2025-10-01 15:14:42 -0500

    Adding Lecture 11

---
+++ Lecture_11/Q1.py
@@ -0,0 +1,23 @@
+x = 0
+
+while x <= 11:
+    #print("Hello!")
+    print(x)
+    x += 1
+ 
+
+
+
+
+
+
+
+
+# print(x)
+    
+# a = 0
+# while a < 12:
+#     print("Hello",a)
+#     a += 1
+# 
+# print(a)
